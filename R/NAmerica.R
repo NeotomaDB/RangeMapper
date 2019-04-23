0a426e02146db1b445639637d4998843e49cfd32
SydWidell <widell@wisc.edu>
2019-04-23 10:17:22 -0500

    added north am herb species

--- R/NAmerica.R
+++ R/NAmerica.R
@@ -16,12 +16,22 @@ library(neotoma)
 # Australian pollen taxa: Nothofagus, Eucalyptus, Casuarina, Callitris, Pyllocladus
 # Mammal taxa: Bison, Smilodon/Panthera, Equus, Rodentia, Mammut, Mammuthus
 
+##TREES
+
+# Records dataset metadata for the below taxon in specifiied location and 
+alnus_datasets <- get_dataset(taxonname = 'Alnus*',
+                              loc = c(-130, 24, -34, 65), 
+                              ageyoung = 0, 
+                              ageold = 21000)
+# Creates list of dataset numbers
+alnus_dataset_numbers <- as.numeric(names(alnus_datasets))
+
 # Records dataset metadata for the below taxon in specifiied location and 
 fagus_datasets <- get_dataset(taxonname = 'Fagus*',
                               loc = c(-130, 24, -34, 65), 
                               ageyoung = 0, 
                               ageold = 21000)
-# Creates list of picea dataset numbers
+# Creates list of dataset numbers
 fagus_dataset_numbers <- as.numeric(names(fagus_datasets))
 
 # Records dataset metadata for the below taxon in specifiied location and 
@@ -33,11 +43,20 @@ picea_datasets <- get_dataset(taxonname = 'Picea*',
 picea_dataset_numbers <- as.numeric(names(picea_datasets))
 
 # Records dataset metadata for the below taxon in specifiied location and 
+pinus_datasets <- get_dataset(taxonname = 'Pinus*',
+                              loc = c(-130, 24, -34, 65), 
+                              ageyoung = 0, 
+                              ageold = 21000)
+# Creates list of picea dataset numbers
+
+pinus_dataset_numbers <- as.numeric(names(pinus_datasets))
+
+# Records dataset metadata for the below taxon in specifiied location and 
 quercus_datasets <- get_dataset(taxonname = 'Quercus*',
                               loc = c(-130, 24, -34, 65), 
                               ageyoung = 0, 
                               ageold = 21000)
-# Creates list of quercus dataset numbers
+# Creates list of dataset numbers
 quercus_dataset_numbers <- as.numeric(names(quercus_datasets))
 
 # Records dataset metadata for the below taxon in specifiied location and 
@@ -45,7 +64,7 @@ spruce_datasets <- get_dataset(taxonname = 'Spruce*',
                               loc = c(-130, 24, -34, 65), 
                               ageyoung = 0, 
                               ageold = 21000)
-# Creates list of spruce dataset numbers
+# Creates list of  dataset numbers
 spruce_dataset_numbers <- as.numeric(names(spruce_datasets))
 
 # Records dataset metadata for the below taxon in specifiied location and 
@@ -53,18 +72,69 @@ tsuga_datasets <- get_dataset(taxonname = 'Tsuga*',
                                loc = c(-130, 24, -34, 65), 
                                ageyoung = 0, 
                                ageold = 21000)
-# Creates list of tsuga dataset numbers
+# Creates list of dataset numbers
 tsuga_dataset_numbers <- as.numeric(names(tsuga_datasets))
+
+# Records dataset metadata for the below taxon in specifiied location and 
+ulmus_datasets <- get_dataset(taxonname = 'Ulmus*',
+                              loc = c(-130, 24, -34, 65), 
+                              ageyoung = 0, 
+                              ageold = 21000)
+# Creates list of dataset numbers
+ulmus_dataset_numbers <- as.numeric(names(ulmus_datasets))
+
+##HERBACEOUS 
+
+# Records dataset metadata for the below taxon in specifiied location and 
+#asteraceae_datasets <- get_dataset(taxonname = 'Asteraceae*',
+                                   #loc = c(-130, 24, -34, 65), 
+                                   #ageyoung = 0, 
+                                   #ageold = 21000)
+# Creates list of dataset numbers
+#asteraceae_dataset_numbers <- as.numeric(names(asteraceae_datasets))
+
+# Records dataset metadata for the below taxon in specifiied location and 
+ambrosia_datasets <- get_dataset(taxonname = 'Ambrosia*',
+                                   loc = c(-130, 24, -34, 65), 
+                                   ageyoung = 0, 
+                                   ageold = 21000)
+# Creates list of dataset numbers
+ambrosia_dataset_numbers <- as.numeric(names(ambrosia_datasets))
+
+# Records dataset metadata for the below taxon in specifiied location and 
+cyperaceae_datasets <- get_dataset(taxonname = 'Cyperaceae*',
+                                loc = c(-130, 24, -34, 65), 
+                                ageyoung = 0, 
+                                ageold = 21000)
+# Creates list of dataset numbers
+cyperaceae_dataset_numbers <- as.numeric(names(cyperaceae_datasets))
+
+
+poaceae_datasets <- get_dataset(taxonname = 'Poaceae*',
+                              loc = c(-130, 24, -34, 65), 
+                              ageyoung = 0, 
+                              ageold = 21000)
+# Creates list of dataset numbers
+poaceae_dataset_numbers <- as.numeric(names(poaceae_datasets))
 
 # Creates empty list 
 site_dataset_numbers = list()
 
 # Combines lists of different taxa dataset numbers
-site_dataset_numbers <- append(site_dataset_numbers, c(fagus_dataset_numbers,
+site_dataset_numbers <- append(site_dataset_numbers, c(#TREES
+                                                       alnus_dataset_numbers,
+                                                       fagus_dataset_numbers,
                                                        picea_dataset_numbers,
+                                                       pinus_dataset_numbers,
                                                        quercus_dataset_numbers, 
                                                        tsuga_dataset_numbers,
-                                                       spruce_dataset_numbers)) 
+                                                       spruce_dataset_numbers,
+                                                       ulmus_dataset_numbers,
+                                                       #HERBS
+                                                       ambrosia_dataset_numbers, 
+                                                       #astercaceae_dataset_numbers,
+                                                       cyperaceae_dataset_numbers,
+                                                       poaceae_dataset_numbers)) 
 
 # Removes duplicate dataset numbers
 all_dataset_numbers <- as.numeric(unique(site_dataset_numbers))
